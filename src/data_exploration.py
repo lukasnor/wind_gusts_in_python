ce8b4d66aa98fb6e4cdb632393d45cb7ecbea11d
Lukas Norkowski <lukasnorkowski@yahoo.de>
2021-11-26 13:44:21 +0100

    EmosModel.py: fit function implemented and R integration introduced. Not tested nor complete. scoringRules.R: Should import the needed functionality

--- src/data_exploration.py
+++ src/data_exploration.py
@@ -1,8 +1,19 @@
+import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 
 if __name__ == "__main__":
-    train_data = pd.read_csv("../data/df_train.csv", index_col=0)
-    test_data = pd.read_csv("../data/df_test.csv", index_col=0)
+    train_data = pd.read_csv("data/df_train.csv", index_col=0)
+    # test_data = pd.read_csv("data/df_test.csv", index_col=0)
+
+    # train_data.info()
 
     loc1 = train_data[train_data["location"] == 1]  # only data from location 1
-    train_data.groupby("location")["ens_mean"].mean()
+    # train_data.groupby("location")["ens_mean"].mean()
+    #loc1.info()
+    #loc1.iloc[:,7:27].plot()
+    #plt.show()
+    test_data = pd.DataFrame({"loc":loc1.iloc[0],"obs":loc1.iloc[1]})
+    test_data.info()
+    npa =np.array(loc1.iloc[1])
+    print(npa.mean())

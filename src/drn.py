1fb370bbbcc9c959debb2aafd06a155c4873d504
Lukas Norkowski <lukasnorkowski@yahoo.de>
2022-01-14 14:22:06 +0100

    drn.py: Custom Loss works .gitignore: added things

--- src/drn.py
+++ src/drn.py
@@ -1,4 +1,6 @@
 # Imports
+import datetime
+import keras.losses
 import pandas as pd
 import tensorflow as tf
 from keras import layers, Model
@@ -7,7 +9,7 @@ import tensorflow_probability as tfp
 from keras.callbacks import EarlyStopping
 
 #  Import standard logistic distribution for the custom loss
-tfd = tfp.distributions.Logistic(1, 0)
+tfd = tfp.distributions.Logistic(0, 1)
 
 
 # Custom loss function
@@ -17,10 +19,8 @@ def custom_loss(y_true, y_pred):
     mu = y_pred[0]
     # sigma = np.dot(y_pred, np.array([[0], [1]]))
     sigma = y_pred[1]
-
     # Truncated in zero
     z = tf.maximum(0.0, y_true)
-
     # Standardization
     z_0 = -mu / sigma  # q ~= -18.4 -> p = 1e-8
     z_y = (z - mu) / sigma
@@ -102,17 +102,23 @@ test_data = test_data.loc[:, train_vars]
 
 # Number of direct predictants w.o. location
 dir_pred_vars = train_vars.drop(["location", "obs"])
-print(dir_pred_vars)
 
 # Split data into training and validation
 # val_data = train_data[-10:] # not necessary with validation_split in model.fit()
 
-# Scale dir_preds w/o location
-
 # Test model right here
-model = get_keras_model(n_dir_preds=len(dir_pred_vars), n_loc=len(train_data["location"].unique()),
-                        emb_dim=hpar_ls["emb_dim"], lay1=hpar_ls["lay1"], actv=hpar_ls["actv"])
-model.compile(optimizer="adam", loss=custom_loss)
+model = get_keras_model(n_dir_preds=len(dir_pred_vars),
+                        n_loc=len(train_data["location"].unique()),
+                        emb_dim=hpar_ls["emb_dim"],
+                        lay1=hpar_ls["lay1"],
+                        actv=hpar_ls["actv"])
+model.compile(optimizer="adam", loss=custom_loss)  # vielleicht anderen Loss probieren
+# model.compile(optimizer="adam",loss=keras.losses.mean_squared_error)
 # Compile the model
-history = model.fit([train_data["location"], train_data[dir_pred_vars]], train_data["obs"], batch_size=100, epochs=10,
-                    validation_split=0.1, callbacks=EarlyStopping(patience=hpar_ls["n_patience"]))
+history = model.fit([train_data["location"],
+                     train_data[dir_pred_vars]],
+                    train_data["obs"],
+                    batch_size=hpar_ls["n_batch"],
+                    epochs=hpar_ls["n_epochs"],
+                    validation_split=0.1,
+                    callbacks=EarlyStopping(patience=hpar_ls["n_patience"]))

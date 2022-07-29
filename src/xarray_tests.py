6c34b14adca92844e2d638c077baa9a0a47b1495
Lukas Norkowski <lukasnorkowski@yahoo.de>
2022-07-29 11:03:13 +0200

    Yay, I understood xarray (see xarray_tests.py)

--- src/xarray_tests.py
+++ src/xarray_tests.py
@@ -1,10 +1,8 @@
-import datetime
-
 import numpy as np
 import pandas as pd
 import xarray as xr
 
-if __name__ == "__main__":
+def try_out():
     n = 10
     locations = np.random.rand(n)
     scales = np.random.rand(n)
@@ -14,7 +12,8 @@ if __name__ == "__main__":
     # und "location" oder "scale" (gebe mir die jeweilige Spalte der Matrix)
     f = xr.DataArray(data=np.array([locations, scales]).transpose()
                      , dims=["time", "features"]
-                     , coords={"location": (["time"], locations), "scale": (["time"], scales), "time": times}
+                     , coords={"location": (["time"], locations), "scale": (["time"], scales),
+                               "time": times}
                      # name: ([liste von dimensionen, die übrig bleiben und in "dims" stehen], DataArray)
                      )
 
@@ -23,12 +22,24 @@ if __name__ == "__main__":
 
     print(g)
     print(g.shape)
-    print(g.loc[:,"location"])
-    print(g.loc[times[1],:])
+    print(g.loc[:, "location"])
+    print(g.loc[times[1], :])
     print(g.sel(time=times[1]))
     print(g["time"])
-    #print(f[0, :])
-    #print(f["location"])
-    #print(f["scale"])
-    #print(f["location"][3:5])
+    # print(f[0, :])
+    # print(f["location"])
+    # print(f["scale"])
+    # print(f["location"][3:5])
     # print(f[3:5, "location"])  # geht nicht
+
+
+def this_works_well():
+    df = pd.read_csv("../data/Sweden_Zone3_Ensembles.csv")
+    df["time"] = pd.to_datetime(df["time"]) - pd.to_timedelta(df["horizon"], "h")
+    df = df.drop(["is_origin"], axis=1)
+    df = df.pivot(index=["time", "horizon", "number"], columns=[])
+    a = df.to_xarray()
+    a.to_netcdf("../data/test.nc")
+
+if __name__ == "__main__":
+    a = xr.load_dataset("../data/test.nc")
